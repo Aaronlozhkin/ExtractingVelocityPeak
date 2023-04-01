f7caa189684acb06b8ddad8581fd69cbd11b1b76
Aaronlozhkin <Aaronlozhkin@gmail.com>
2023-03-31 21:35:03 -0400

    Update velocityCleaner
    Added optimizations to velocityCleaner to run smoother. Placeholder variable implemented for outlier replacement.

--- velocityCleaner.m
+++ velocityCleaner.m
@@ -1,21 +1,28 @@
 function f = velocityCleaner(vSpectrogram)
+    
+    % Identifies outlier values in a velocity value and removes values
+    % around that outlier until it levels out at the median.
+    
     average = median(vSpectrogram);
-    locs = find(vSpectrogram < 10 | vSpectrogram > 150);
+    placeHolder = NaN;
+    locs = find(vSpectrogram < -50 | vSpectrogram > 300);
 
     if ~isempty(locs)
+        
+        band = find(vSpectrogram < average+2 & vSpectrogram > average-2);
 
         for i = 1 : length(locs)
-            index = find(vSpectrogram < average+2 & vSpectrogram > average-2) - locs(i);
+            index = band - locs(i);
 
             id1 = max(index(index < 0));
             id2 = min(index(index > 0));
 
             if (isempty(id1))
-                vSpectrogram(1:id1+locs(i)) = average;
+                vSpectrogram(1:id1+locs(i)) = placeHolder;
             elseif (isempty(id2))
-                vSpectrogram(id1+locs(i):end) = average;
+                vSpectrogram(id1+locs(i):end) = placeHolder;
             else
-                vSpectrogram(id1 + locs(i):id2+locs(i)) = average; 
+                vSpectrogram(id1 + locs(i):id2+locs(i)) = placeHolder; 
             end
         end
     end
